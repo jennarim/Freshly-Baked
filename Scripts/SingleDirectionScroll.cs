da7dbc509a2de2f51b0bd1c350dd7ab55171a30f
jennarim <rim.jenna@yahoo.com>
2019-08-31 00:23:53 -0400

    added single directional scrolling script

---
+++ Scripts/SingleDirectionScroll.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingleDirectionScroll : MonoBehaviour
+{
+    public GameObject content;
+    
+    private RectTransform rectT;
+    private float maxPosX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rectT = content.GetComponent<RectTransform>();
+        maxPosX = rectT.anchoredPosition.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float currPosX = rectT.anchoredPosition.x;
+        if (currPosX > maxPosX)
+        {
+            Vector3 newPos = new Vector3(maxPosX, 0, 0);
+            rectT.anchoredPosition = newPos;
+        } else if (currPosX < maxPosX)
+        {
+            maxPosX = currPosX;
+        }
+    }
+}
